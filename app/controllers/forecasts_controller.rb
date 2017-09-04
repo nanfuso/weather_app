d6d3cad1af7e37306f0527557542fb7f9b51af2c
natail <nataleanfuso@gmail.com>
2017-09-04 18:03:05 -0500

    final changes

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -2,19 +2,28 @@ class ForecastsController < ApplicationController
 
     def index
         @zip_code = params[:zip_code] || "60604"
-        @forecasts = Forecast.where(zip_code: @zip_code) 
-        unless @forecasts.length != 0
-            @forecasts = Unirest.get("#{ ENV["API_HOST"]}/#{ ENV["API_TOKEN"] }/forecast/q/#{ @zip_code }.json").body["forecast"]["simpleforecast"]["forecastday"].first(3)
-            @forecasts.each do |response|
-                forecast = Forecast.new(
+
+        if Forecast.where("zip_code = ? and updated_at >= ?", @zip_code, DateTime.now - 1.hour).length == 0
+            @responses = Unirest.get("#{ ENV["API_HOST"]}/#{ ENV["API_TOKEN"] }/forecast/q/#{ @zip_code }.json").body["forecast"]["simpleforecast"]["forecastday"].first(3)
+            @responses.each do |response|
+                new_forecast = Forecast.new(
                                             zip_code: @zip_code,
                                             day: response["date"]["pretty"],
                                             high: response["high"]["fahrenheit"],
                                             low: response["low"]["fahrenheit"],
                                             description: response["conditions"]
                                             )
-                forecast.save
+                new_forecast.save
             end
+            @forecasts = Forecast.where(zip_code: @zip_code)    
+
+        else 
+            @forecasts = Forecast.where(zip_code: @zip_code).last(3)
+
+            stale_forecasts = Forecast.where(zip_code: @zip_code).first(3)
+            stale_forecasts.each do |stale|
+                stale.destroy
+            end        
         end
     end
 end
